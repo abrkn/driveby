2ce439610c5aa58d691790546711219dfa18ab47
Andreas Brekken <andreas@brekken.com>
2018-10-16 19:35:39 +0700

    add concurrency for tx

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const safep = require('safep');
 const bitcoin = require('bitcoin');
 const Promise = require('bluebird');
 const redis = require('redis');
+const pMap = require('p-map');
+
 Promise.promisifyAll(redis);
 
 function urlToBitcoinOptions(url) {
@@ -17,6 +19,7 @@ function urlToBitcoinOptions(url) {
 
 const { SOURCE_BITCOIND_URL, DESTINATION_BITCOIND_URL, REDIS_URL } = process.env;
 const SOURCE_MIN_HEIGHT = +(process.env.SOURCE_MIN_HEIGHT || 543543);
+const TX_CONCURRENCY = 10;
 
 const sourceBitcoinRpc = new bitcoin.Client(urlToBitcoinOptions(new URL(SOURCE_BITCOIND_URL)));
 safep.applyTo(sourceBitcoinRpc, 'cmd');
@@ -27,41 +30,35 @@ safep.applyTo(destBitcoinRpc, 'cmd');
 const redisConn = redis.createClient(REDIS_URL);
 
 const main = async () => {
-  // const help = await sourceBitcoinRpc.cmdAsync('help');
-  // console.log(help);
-  // assert();
-
   const prevHeight = +(await redisConn.getAsync('driveby.height')) || SOURCE_MIN_HEIGHT;
   const { blocks: sourceHeight } = await sourceBitcoinRpc.cmdAsync('getblockchaininfo');
+
+  const submitTx = async (height, tx, index) => {
+    if (+index === 0) {
+      return;
+    }
+
+    const { hex: txHex, hash: txHash } = tx;
+
+    const [error] = await destBitcoinRpc.cmdSafe('sendrawtransaction', txHex);
+
+    if (error) {
+      console.error(height, txHash, 'ERROR', error.message);
+      return;
+    }
+
+    console.log(height, txHash, 'OK');
+  };
 
   for (let height = prevHeight + 1; height <= sourceHeight; height++) {
     const blockHash = await sourceBitcoinRpc.cmdAsync('getblockhash', height);
     const blockWithTxs = await sourceBitcoinRpc.cmdAsync('getblock', blockHash, 2);
     const { tx: blockTxs } = blockWithTxs;
 
-    for (const txIndex in blockTxs) {
-      if (+txIndex === 0) {
-        continue;
-      }
-
-      const tx = blockTxs[txIndex];
-
-      const { hex: txHex, hash: txHash } = tx;
-
-      const [error] = await destBitcoinRpc.cmdSafe('sendrawtransaction', txHex);
-
-      if (error) {
-        console.error(height, txHash, 'ERROR', error.message);
-      } else {
-        console.log(height, txHash, 'OK');
-      }
-    }
+    await pMap(blockTxs, (tx, index) => submitTx(height, tx, index), { concurrency: TX_CONCURRENCY });
 
     await redisConn.setAsync('driveby.height', height);
   }
-
-  // const help = await sourceBitcoinRpc.cmdAsync('help');
-  // console.log(help);
 };
 
 main().then(process.exit);
